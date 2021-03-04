7a88e77f14490a898e634a051b8d87b024fb45d6
Sr Zero <70704341+ferafael@users.noreply.github.com>
2021-03-03 21:29:16 -0300

    Update Local.vue
    Melhorar o local

--- src/components/Local.vue
+++ src/components/Local.vue
@@ -1,10 +1,6 @@
 <template>
-<div>
-    <div class="form__title">
-       <p>Venha nos </p>
-       <span>visitar?</span>
-    </div>
-    <div class="local">
+<div class="row" id="contact">
+    <div class="row" style="height:550px">
       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14876.711386658228!2d-47.8351129!3d-21.2247951!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd33acc9c11ff64eb!2sLim%C3%A3o%20Cravo%20-%20Estudio%20Criativo!5e0!3m2!1sen!2sbr!4v1614698919855!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </div>
